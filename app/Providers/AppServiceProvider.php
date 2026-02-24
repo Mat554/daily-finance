308aba0ed96f70d146781e5350f165fb5c7f2737
Mat554 <matiusrizky25@gmail.com>
2026-02-24 16:32:18 +0700

    Fix: Vercel read-only filesystem storage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,20 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+public function boot(): void
+{
+    // Fix for Vercel Read-Only Filesystem
+    if (env('APP_ENV') === 'production') {
+        $path = '/tmp/storage/framework/';
+        config([
+            'view.compiled' => $path . 'views',
+            'cache.stores.file.path' => $path . 'cache',
+            'session.files' => $path . 'sessions',
+        ]);
+
+        if (!is_dir($path . 'views')) {
+            mkdir($path . 'views', 0755, true);
+        }
     }
+}
 }
