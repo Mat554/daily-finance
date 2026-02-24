bb9e0c3bd6ff89975dfd4863197b045a56ce5fda
Mat554 <matiusrizky25@gmail.com>
2026-02-24 16:39:50 +0700

    Add .gitkeep to ensure storage folders exist on Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,5 @@
 <?php
+
 // Forward Vercel requests to Laravel's normal starting point
+// We use realpath to ensure the serverless function finds the right folder
 require __DIR__ . '/../public/index.php';
