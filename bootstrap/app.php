c4403f579bc0023a7f62eaa7dc70c1d01993beb2
Mat554 <matiusrizky25@gmail.com>
2026-02-24 17:02:20 +0700

    Fix: Corrected bootstrap order and routing for Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,5 @@
 <?php
 
-if (isset($_SERVER['VERCEL_URL'])) {
-    $app->useStoragePath('/tmp/storage');
-}
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,9 +10,16 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->registered(function ($app) {
+        // This is the correct way to fix storage for Vercel
+        if (isset($_SERVER['VERCEL_URL'])) {
+            $app->useStoragePath('/tmp/storage');
+        }
+    })
+    ->create();
